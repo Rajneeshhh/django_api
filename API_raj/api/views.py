7daf8589a251799cbaf10a51ecef1fbfc78f241d
Rajneesh <ssrajneesh693@gmail.com>
2023-09-07 15:13:07 +0530

    company

---
+++ API_raj/api/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from api.models import Company
+from api.serializers import Companyserializer
+# Create your views here.
+class Companyviewset(viewsets.ModelViewSet):
+    queryset=Company.objects.all()
+    serializer_class = Companyserializer
